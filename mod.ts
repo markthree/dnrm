63ff41dcec727b8e055d6743ff9f3895316b0e23
markthree <1801982702@qq.com>
2023-05-11 19:27:30 +0800

    chore: docs update

--- mod.ts
+++ mod.ts
@@ -57,7 +57,7 @@ if (import.meta.main) {
   await new Command()
     .usage("[command|option]")
     .name("dnrm")
-    .version("0.4.1")
+    .version("0.4.2")
     .description("deno 实现的 nrm，每次切换源都在 100ms 内，速度超级快")
     .action(async () => {
       const { currentRegistry } = await getConfig();
